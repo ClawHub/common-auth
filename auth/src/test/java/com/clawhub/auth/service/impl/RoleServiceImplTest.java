c85c0b3a3ba63670d4ced1547a8f440dc92afc7e
李志明 <18120177709@163.com>
2018-09-11 00:02:03 +0800

    注释掉测试文件

--- auth/src/test/java/com/clawhub/auth/service/impl/RoleServiceImplTest.java
+++ auth/src/test/java/com/clawhub/auth/service/impl/RoleServiceImplTest.java
@@ -1,26 +1,26 @@
-package com.clawhub.auth.service.impl;
-
-import com.clawhub.auth.AuthApp;
-import com.clawhub.auth.service.RoleService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Set;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = AuthApp.class)
-public class RoleServiceImplTest {
-    @Autowired
-    private RoleService roleService;
-
-    @Test
-    public void findRolesByUser() {
-        Set<String> roleNames = roleService.findRoleNamesByUserId("0");
-        for (String roleName : roleNames) {
-            System.out.println(roleName);
-        }
-    }
-}
+//package com.clawhub.auth.service.impl;
+//
+//import com.clawhub.auth.AuthApp;
+//import com.clawhub.auth.service.RoleService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.Set;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = AuthApp.class)
+//public class RoleServiceImplTest {
+//    @Autowired
+//    private RoleService roleService;
+//
+//    @Test
+//    public void findRolesByUser() {
+//        Set<String> roleNames = roleService.findRoleNamesByUserId("0");
+//        for (String roleName : roleNames) {
+//            System.out.println(roleName);
+//        }
+//    }
+//}
--- auth/src/test/java/com/clawhub/auth/service/impl/UserServiceImplTest.java
+++ auth/src/test/java/com/clawhub/auth/service/impl/UserServiceImplTest.java
@@ -1,28 +1,28 @@
-package com.clawhub.auth.service.impl;
-
-import com.clawhub.auth.AuthApp;
-import com.clawhub.auth.entity.SysUser;
-import com.clawhub.auth.service.UserService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = AuthApp.class)
-public class UserServiceImplTest {
-
-    @Autowired
-    private UserService userService;
-
-    @Test
-    public void findByUsername() {
-        SysUser sysUser = userService.findByUsername("admin");
-        System.out.println(sysUser.getPassword());
-    }
-
-    @Test
-    public void queryAllSysUser() {
-    }
-}
+//package com.clawhub.auth.service.impl;
+//
+//import com.clawhub.auth.AuthApp;
+//import com.clawhub.auth.entity.SysUser;
+//import com.clawhub.auth.service.UserService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = AuthApp.class)
+//public class UserServiceImplTest {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Test
+//    public void findByUsername() {
+//        SysUser sysUser = userService.findByUsername("admin");
+//        System.out.println(sysUser.getPassword());
+//    }
+//
+//    @Test
+//    public void queryAllSysUser() {
+//    }
+//}
